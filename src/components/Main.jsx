123b85f86ce2d91ae3fc081f17cfff7793afe2e1
Rohit <kumarohit1202@gmail.com>
2024-10-15 15:17:38 +0530

    made some changes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -25,6 +25,9 @@ function Main() {
 
         const handleDoubleClick = (event) => {
             if (cardRef.current && !cardRef.current.contains(event.target)) {
+                setTitle('');
+                setContent('');
+                setIsPublic(false);
                 setExpanded(false);
             }
         };
@@ -117,7 +120,11 @@ function Main() {
                                 ></textarea>
                             </div>
                             <div className="mt-4 flex justify-end">
-                                <button className={`${isPublic == true ? 'bg-green-500 hover:bg-green-600' : 'bg-yellow-500 hover:bg-yellow-600'} text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500`} onClick={handleSaveNote}>
+                                <button 
+                                    className={`${isPublic ? 'bg-green-500 hover:bg-green-600' : 'bg-yellow-500 hover:bg-yellow-600'} text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${!title.trim() ? 'opacity-50 cursor-not-allowed' : ''}`} 
+                                    onClick={handleSaveNote}
+                                    disabled={!title.trim()}
+                                >
                                     Save Note
                                 </button>
                             </div>
